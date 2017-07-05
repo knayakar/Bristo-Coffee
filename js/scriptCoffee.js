9ec8d5090790cbbed008f3701a0ff962947762a9
Kartik Nayakar <nayakarkarthik53@gmail.com>
2017-07-05 14:09:30 +0530

    First push

---
+++ js/scriptCoffee.js
@@ -0,0 +1,83 @@
+$(function() {
+   $(".mcoffee").hover(function() {
+       $(".mcoffee").addClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".mcoffee").mouseleave(function() {
+       $(".mcoffee").removeClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".mtea").hover(function() {
+       $(".mtea").addClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".mtea").mouseleave(function() {
+       $(".mtea").removeClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".mbreakfast").hover(function() {
+       $(".mbreakfast").addClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".mbreakfast").mouseleave(function() {
+       $(".mbreakfast").removeClass('animated infinite tada');
+   }) ;
+});
+
+$(function() {
+   $(".team1").hover(function() {
+       $(".team1").addClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team1").mouseleave(function() {
+       $(".team1").removeClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team2").hover(function() {
+       $(".team2").addClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team2").mouseleave(function() {
+       $(".team2").removeClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team3").hover(function() {
+       $(".team3").addClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team3").mouseleave(function() {
+       $(".team3").removeClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team4").hover(function() {
+       $(".team4").addClass('animated infinite swing');
+   }) ;
+});
+
+$(function() {
+   $(".team4").mouseleave(function() {
+       $(".team4").removeClass('animated infinite swing');
+   }) ;
+});
